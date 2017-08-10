a8369b5d238e93e77783da17a89864b3d0c40ed9
rajeev singh <rajeev.chauhan08@gmail.com>
2017-08-10 20:08:16 +0530

    added getters setters

--- src/main/java/restapitest/AssignmentData.java
+++ src/main/java/restapitest/AssignmentData.java
@@ -9,4 +9,25 @@ public class AssignmentData {
 	private String id;
 	private AssigAttribute attributes;
 	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public AssigAttribute getAttributes() {
+		return attributes;
+	}
+	public void setAttributes(AssigAttribute attributes) {
+		this.attributes = attributes;
+	}
+	
+	
+	
 }
